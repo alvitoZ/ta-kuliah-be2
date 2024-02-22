30cd306422143080804266def3ceaf781d72a775
alvitoZ <alvitozidane76@gmail.com>
2024-02-22 17:06:29 +0700

    first commit

---
+++ src/controller/ImageController.ts
@@ -0,0 +1,107 @@
+import { Request, Response, NextFunction } from "express";
+import RemoveImage from "../utils/RemoveImage";
+import { ImageModel } from "../models/ImageModel";
+import path from "path";
+
+class ImageController {
+  async getImages(req: Request, res: Response): Promise<any> {
+    const { role } = res.locals.user;
+    try {
+      if (role === "siswa") {
+        return res.status(403).json({
+          msg: "siswa cannot access getImages",
+        });
+      }
+      const result = await ImageModel.find();
+      return res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      return res.status(503).json({
+        msg: `error`,
+        data: [],
+      });
+    }
+  }
+  async postImage(req: Request, res: Response): Promise<any> {
+    const { fullname, role } = res.locals.user;
+
+    try {
+      if (role === "siswa") {
+        return res.status(403).json({
+          msg: "siswa cannot access postGambar",
+        });
+      }
+
+      let { alt } = req.body;
+      const image: string = req.file?.filename || "default";
+
+      const allowedExtensions = [".jpg", ".jpeg"];
+      const ext = path.extname(req.file?.originalname || "").toLowerCase();
+
+      if (!allowedExtensions.includes(ext)) {
+        return res.status(400).json({
+          msg: "Invalid file extension. Allowed extensions: .jpg, .jpeg, .png",
+        });
+      }
+
+      const gambar = await ImageModel.insertMany({
+        image: image,
+        alt: alt,
+      });
+
+      return res.status(200).json({
+        msg: "create materi berhasil",
+        data: gambar,
+      });
+    } catch (error) {
+      return res.status(503).json({
+        msg: `error`,
+        data: [],
+      });
+    }
+  }
+
+  async deleteImage(req: Request, res: Response): Promise<any> {
+    const { role } = res.locals.user;
+    try {
+      if (role === "siswa") {
+        return res.status(403).json({
+          msg: "siswa cannot access deleteImage",
+        });
+      }
+      const result = await ImageModel.findByIdAndDelete({ _id: req.params.id });
+      RemoveImage(result?.image);
+      return res.status(200).json({
+        data: result,
+      });
+    } catch (error) {
+      return res.status(503).json({
+        msg: `error`,
+        data: [],
+      });
+    }
+  }
+
+  async deleteImageFile(req: Request, res: Response): Promise<any> {
+    const { role } = res.locals.user;
+    try {
+      if (role === "siswa") {
+        return res.status(403).json({
+          msg: "siswa cannot access deleteImage",
+        });
+      }
+      RemoveImage(req.params.image);
+      return res.status(200).json({
+        data: "success",
+      });
+    } catch (error) {
+      return res.status(503).json({
+        msg: `error`,
+        data: [],
+      });
+    }
+  }
+}
+
+export default new ImageController();
---
+++ src/middleware/AuthValidator.ts
@@ -0,0 +1,55 @@
+{/*import { Request, Response, NextFunction } from "express";
+import { check, validationResult } from "express-validator";
+// import cekNama from "../utils/IncludeSlash";
+
+const validate = [
+ /* check("email")
+    .isLength({ min: 4 })
+    .custom(async (value) => {
+      /* const cekSlash: boolean = cekNama(value);
+      if (cekSlash) {
+        throw new Error("dont use /");
+      }
+      return true;
+    }),
+  check("password").isLength({ min: 4 }),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422);
+      return res.send({
+        errors: errors.array(),
+      });
+    } else {
+      return next();
+    }
+  },
+];
+
+export default validate;*/}
+
+
+import { Request, Response, NextFunction } from "express";
+import { check, validationResult } from "express-validator";
+
+const validate = [
+  check("email")
+    .isLength({ min: 4 })
+    .custom(async (value) => {
+
+    }),
+  check("password").isLength({ min: 4 }),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422);
+      return res.send({
+        errors: errors.array(),
+      });
+    } else {
+      return next();
+    }
+  },
+];
+
+export default validate;
